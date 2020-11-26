e0d5bdee022cc053952c764850356ea75588a0fb
Biggunz <poundmaking@gmail.com>
2020-11-26 21:00:43 +0000

    first commit

---
+++ 1.js
@@ -0,0 +1,30 @@
+
+'use strict';
+
+console.log(puta);
+var puta = 1;
+let second = 2;
+const pi = 3.14;
+
+
+let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+//let arr = ['plum.png','orange.png','apple.png'];
+//console.log(arr[0]);
+//console.log(person.name);
+//let answer = confirm('are you 18?')
+//console.log(answer);
+//let answer2 = prompt('are you 18?', 'yes');
+//console.log(typeof(null));
+console.log('arr' + 'object');
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(20%8);
